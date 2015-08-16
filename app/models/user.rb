7e03be622706651122be8bb6804cef43e5fc1d7d
catcher1 <danlau168@gmail.com>
2015-08-16 14:34:00 -0700

    Finish authentication and posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
 class User < ActiveRecord::Base
+  has_many :posts
+  before_create :create_remember_token
   has_secure_password
+
+  def self.new_remember_token #class method
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.encrypt(token) #class method
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+  def create_remember_token #calling class methods inside and instance method
+    self.remember_token = User.encrypt(User.new_remember_token)
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150815041544) do
+ActiveRecord::Schema.define(version: 20150816004236) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+  end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +29,9 @@ ActiveRecord::Schema.define(version: 20150815041544) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "remember_token"
   end
+
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
